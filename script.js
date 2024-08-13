cb54e62201b31731d8dc18b096fa37f9afca710d
Carololivert <carololivert@gmail.com>
2024-08-13 09:33:47 -0300

    aca

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let titulo = document.getElementById("titulo").value;
-let nome = document.getElementById("nome").value;
 
 console.log(titulo);
-console.log(nome);
 
 function submeter () {
-    let nome = document.getElementById("nome".value)
-    alert ("Estou na funçao submeter")
-    console.log (nome)
+    let nome = document.getElementById("nome").value;
+    let cpf = document.getElementById("cpf").value;
+    let idade = document.getElementById("idade").value;
+    console.log (cpf)
+    console.log(validaCPF(cpf));
 }
+function validaCPF(cpf) {
+    if (cpf = "")
+    alert ("Campo CPF nao pode ser vazio")
+    return false;
+}
+    return  true;
