7e30fbd6b0e41c15fbb1ee44bfea1617c9335fa3
Carololivert <carololivert@gmail.com>
2024-08-13 09:27:08 -0300

    at

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-let dom = document;
-console.log(dom);
+let titulo = document.getElementById("titulo").value;
+let nome = document.getElementById("nome").value;
+
+console.log(titulo);
+console.log(nome);
+
+function submeter () {
+    let nome = document.getElementById("nome".value)
+    alert ("Estou na funçao submeter")
+    console.log (nome)
+}
