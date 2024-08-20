3dda32cbbc6afe4c2088cce1fe653c4d18179065
Carololivert <carololivert@gmail.com>
2024-08-20 10:32:37 -0300

    att

--- script.js
+++ script.js
@@ -28,6 +28,32 @@ function validaCPF(cpf) {
        
     }
 
+    if(cpf.length !=11 )
+
     return true
+
+    let soma = 0;
+
+    for ( let i = 1; i <= 9; i++) {
+        console.log(cpf.charAt(i-1));
+        soma = soma +(cpf.charAt(i-i) * (10 - (i-1)));
+     
+    }
+    console.log(soma);
+    let resto = soma % 11;
+    
+    if(resto < 2) {
+        if(cpf.charAt(9)  !=0) {
+            alert("CPF invalido");
+            return false;
+        }
+    return true
+    }
+
+
+    let digitoVerificador1 =  11 - resto;
+     
+
+
 }
 
